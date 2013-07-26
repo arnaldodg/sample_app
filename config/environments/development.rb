7285f36b867388d023dc1a581147e1e605b73642
Arnaldo Diniz <arnaldodg@gmail.com>
2013-07-26 19:53:38 -0300

    Compiled Assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ SampleApp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.assets.prefix = "/dev-assets"
+  config.assets.initialize_on_precompile = false
 end
